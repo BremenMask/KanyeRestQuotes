3c96e21866927560f9f3fffa7dd523d5f537665e
Bremen Mask <thejames0023@gmail.com>
2020-11-21 20:41:54 -0500

    some styling

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return `
-      <nav class="nav__section">
+      <nav class="nav">
         <section> 
             <h3 class="nav__quote">New Quote</h3>
             <h3 class="nav__api">Kanye Rest API</h3>

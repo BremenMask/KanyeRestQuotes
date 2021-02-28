683888b600cc0203f1d5396fce22e429f512d51e
Bremen Mask <thejames0023@gmail.com>
2021-02-28 13:57:32 -0500

    Made public

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
     return `
       <nav class="nav">
-        <h3 class="nav__quote">New Quote</h3>
-        <h3 class="nav__api">Kanye Rest API</h3>
+        <h3 class="nav__quote">New Quote Flow</h3>
+        <h3 class="nav__api"><a href="https://kanye.rest/">Kanye Rest API</a></h3>
       </nav>
       `
   }
--- src/js/components/Quote.js
+++ src/js/components/Quote.js
@@ -2,7 +2,7 @@ export default function Quote() {
     return `
     <div class="quote-div">
         <p class="kanye-quote"></p>
-        <button class="quote-button">Click</button>
+        <button class="quote-button">Show Me The Answers, Ye</button>
     </div>
     `
 }
